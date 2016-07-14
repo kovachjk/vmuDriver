2bd0f9f87a1e681c42f07111b1c9af8a155d8682
johnk <john@stillbright.com>
2016-07-14 11:00:03 -0500

    Initial commit

---
+++ public/libs/vmuCmdAPI.js
@@ -0,0 +1,6 @@
+/**
+ * Created by johnk on 7/14/2016.
+ * Copyright Stillbright Management LLC 2015
+ * This file defines, builds and verifies VMU commands
+ *
+ */
---
+++ public/libs/vmuDriver.js
@@ -0,0 +1,6 @@
+/**
+ * Created by johnk on 6/24/2016.
+ * Copyright Stillbright Management LLC 2015
+ * This file is referenced in Google Doc's VMU Protocol Project file
+ * It comes from a previous site on RTS2\vmu.
+ */
